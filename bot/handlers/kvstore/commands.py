6601d1b27135d10ba997e7cbf75474b5d4bd64bc
Evgeny Avramenko <kanst9@yandex-team.ru>
2025-01-08 15:31:57 +0300

    Return kv items

--- bot/handlers/kvstore/commands.py
+++ bot/handlers/kvstore/commands.py
@@ -1,3 +1,6 @@
+import time
+import uuid
+
 from telegram import Update
 
 from bot.app.models import KVItem
@@ -20,31 +23,46 @@ def get_cmd(update: Update, context: ECallbackContext):
 
 def list_cmd(update: Update, context: ECallbackContext):
     ans = ""
+
     items = context.db_session.query(KVItem).filter_by(
         chat_id=update.effective_chat.id).all()
+    if not items:
+        update.effective_chat.send_message("no keys yet(")
+
     for i, item in enumerate(items, 1):
-        ans += "{}) {} - {}\n".format(i, item.key, item.value)
-    # Quick fix to avoid "Message too long" error
-    update.message.reply_text(ans[:4000] or "no keys yet(")
+        # ans += "{}) {} - {}\n".format(i, item.key, item.value)
+        line = "{}) {}\n".format(i, item.value)
+        if (len(ans) + len(line)) > 4096:
+            update.effective_chat.send_message(ans)
+            ans = ""
+            time.sleep(1)
+        else:
+            ans += line
+    if ans:
+        update.effective_chat.send_message(ans)
 
 
 def set_cmd(update: Update, context: ECallbackContext):
-    if len(context.args) < 2:
-        update.message.reply_text('provide key and value please')
-        return
+    if context.args[0].startswith('key_'):
+        key = context.args[0]
+        value = " ".join(context.args[1:])
+    else:
+        key = 'key_' + uuid.uuid4().hex[:8]
+        value = " ".join(context.args)
+
     kv_item: KVItem = context.db_session.query(KVItem).filter_by(
         chat_id=update.effective_chat.id,
-        key=context.args[0]).one_or_none()
+        key=key).one_or_none()
     if kv_item is None:
         kv_item = KVItem(chat_id=update.effective_chat.id,
-                         key=context.args[0],
-                         value=" ".join(context.args[1:]))
+                         key=key,
+                         value=value)
     else:
-        kv_item.value = " ".join(context.args[1:])
+        kv_item.value = value
     context.db_session.add(kv_item)
     context.db_session.commit()
     update.message.reply_text(
-        'Key {} successfully added'.format(context.args[0]))
+        'Key {} successfully added'.format(key))
 
 
 def del_cmd(update: Update, context: ECallbackContext):
