91874e75608dfe1737df38160335a5ad5757186c
Evgeny Avramenko <kanst9@yandex-team.ru>
2026-01-05 20:54:02 +0300

    vibe: fix coins

--- bot/setup_commands.py
+++ bot/setup_commands.py
@@ -27,7 +27,6 @@ commands = [
     ('pidorfinalclose', 'close final voting (admins only)'),
     ('pidorcoinsme', 'show your pidorcoin balance'),
     ('pidorcoinsstats', 'show pidorcoin stats for current year'),
-    ('pidorcoinsall', 'show pidorcoin stats for all time'),
     # ('meme', 'get some random meme'),
     # ('memeru', 'get some random russian meme'),
     # ('ttvideo', 'get video from tiktok'),
